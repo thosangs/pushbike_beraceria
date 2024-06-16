e7ac70c33e3fcac60aeb247733664440f896746b
Thosan Seceng <thosan.girisona@gmail.com>
2024-06-16 08:42:58 +0700

    Add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import Header from '../components/header'
 import Footer from '../components/footer'
 import Head from 'next/head';
+import { Analytics } from '@vercel/analytics/react';
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -28,6 +29,7 @@ export default function RootLayout({
         <Header></Header>
         {children}
         <Footer></Footer>
+        <Analytics />
       </body>
     </html>
   );
